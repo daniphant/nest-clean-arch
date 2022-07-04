c6fbe67529c1fb776789df2f83d8a230a9f98cae
daniphant <daniphnt_dev@yahoo.com>
2022-07-04 16:41:15 -0300

    Revert "remove auth strategies"
    This reverts commit 6d961b23b3338a0613d7ad1304a67b156edf6c7f.

--- src/presentation/controllers/app.controller.ts
+++ src/presentation/controllers/app.controller.ts
@@ -1,10 +1,21 @@
-import { Controller, Get, HttpCode, Post, Req } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpCode,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { Public } from 'application/decorators/public.decorator';
+import { AuthService } from 'application/services/auth.service';
 import { Request } from 'express';
 import { Ok } from 'presentation/response-types/success.types';
 
 @Controller()
 export class AppController {
+  constructor(private readonly _authService: AuthService) {}
+
   @Public()
   @Get()
   getHello() {
@@ -12,9 +23,11 @@ export class AppController {
   }
 
   @Public()
+  @UseGuards(AuthGuard('local'))
   @Post('/auth/login')
   @HttpCode(200)
   async login(@Req() req: Request) {
-    return Ok({});
+    const loginData = await this._authService.login(req.user);
+    return Ok(loginData);
   }
 }
--- src/presentation/modules/app.module.ts
+++ src/presentation/modules/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { APP_FILTER } from '@nestjs/core';
 import { AppController } from '../controllers/app.controller';
 import { AllExceptionsFilter } from '../filters/all-exceptions.filter';
+import { AuthModule } from './auth.module';
 import { TodoModule } from './todo.module';
 
 @Module({
-  imports: [TodoModule],
+  imports: [TodoModule, AuthModule],
   providers: [
     {
       provide: APP_FILTER,
