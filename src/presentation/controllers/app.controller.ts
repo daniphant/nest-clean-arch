6d961b23b3338a0613d7ad1304a67b156edf6c7f
daniphant <daniphnt_dev@yahoo.com>
2022-07-04 16:33:50 -0300

    remove auth strategies

--- src/presentation/controllers/app.controller.ts
+++ src/presentation/controllers/app.controller.ts
@@ -1,21 +1,10 @@
-import {
-  Controller,
-  Get,
-  HttpCode,
-  Post,
-  Req,
-  UseGuards,
-} from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+import { Controller, Get, HttpCode, Post, Req } from '@nestjs/common';
 import { Public } from 'application/decorators/public.decorator';
-import { AuthService } from 'application/services/auth.service';
 import { Request } from 'express';
 import { Ok } from 'presentation/response-types/success.types';
 
 @Controller()
 export class AppController {
-  constructor(private readonly _authService: AuthService) {}
-
   @Public()
   @Get()
   getHello() {
@@ -23,11 +12,9 @@ export class AppController {
   }
 
   @Public()
-  @UseGuards(AuthGuard('local'))
   @Post('/auth/login')
   @HttpCode(200)
   async login(@Req() req: Request) {
-    const loginData = await this._authService.login(req.user);
-    return Ok(loginData);
+    return Ok({});
   }
 }
--- src/presentation/modules/app.module.ts
+++ src/presentation/modules/app.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { APP_FILTER } from '@nestjs/core';
 import { AppController } from '../controllers/app.controller';
 import { AllExceptionsFilter } from '../filters/all-exceptions.filter';
-import { AuthModule } from './auth.module';
 import { TodoModule } from './todo.module';
 
 @Module({
-  imports: [TodoModule, AuthModule],
+  imports: [TodoModule],
   providers: [
     {
       provide: APP_FILTER,
