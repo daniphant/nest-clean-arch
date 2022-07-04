dbd9c19da59e919acba341a2ad221455d34250c8
daniphant <daniphnt_dev@yahoo.com>
2022-07-04 16:53:40 -0300

    fix: sub value came from payload.id instead of sub

--- src/application/strategies/jwt.strategy.ts
+++ src/application/strategies/jwt.strategy.ts
@@ -14,6 +14,6 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
   }
 
   async validate(payload: any) {
-    return { email: payload.email, sub: payload.id };
+    return { email: payload.email, sub: payload.sub };
   }
 }
